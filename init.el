1afbc80fad95cf5d6054ede5dd7e97e36cd094cf
Takayuki Muranushi <muranushi@gmail.com>
2015-03-22 09:28:54 +0900

    disable CUA shortcut keys.

--- init.el
+++ init.el
@@ -1,7 +1,24 @@
+;; Instructions.
+;;
+;; Type
+;;   M-x package-refresh-contents
+;; When you see a ``Package not found'' error.
+
+
+;; ~/.emacs.d/site-lisp 以下全部読み込み
+(let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
+(require 'init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
+
+
 ;; packages to install
 (defvar my/packages
-  '(init-loader
-    anything
+  '(anything
     auto-complete
     powerline
     quickrun
@@ -19,6 +36,7 @@
     )
   "A list of packages to install from MELPA at launch.")
 
+
 ;; init package.el
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -34,3 +52,6 @@
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
 (setq init-loader-show-log-after-init t)
+
+;; Do not use C-x, C-c, C-v as cut, copy, and paste
+(setq cua-enable-cua-keys  nil)
