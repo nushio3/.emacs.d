bc022296f578c03ef698f35472b150fc98f96bec
Takayuki Muranushi <muranushi@gmail.com>
2015-03-22 09:31:22 +0900

    start using org mode.

--- init.el
+++ init.el
@@ -55,3 +55,9 @@
 
 ;; Do not use C-x, C-c, C-v as cut, copy, and paste
 (setq cua-enable-cua-keys  nil)
+
+;; Org-mode shortcut keys.
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
