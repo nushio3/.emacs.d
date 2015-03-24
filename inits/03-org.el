3eb2f1d0d13f55a92381b515725e7a0471b70736
Takayuki Muranushi <muranushi@gmail.com>
2015-03-24 23:39:44 +0900

    org mode

--- inits/03-org.el
+++ inits/03-org.el
@@ -3,3 +3,14 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+
+;; capture templates
+(setq org-capture-templates
+      '(("p" "Project Task" entry (file+headline (expand-file-name "~/nushiolab/org/project.org") "Inbox")
+             "** TODO %?\n    %i\n    %a\n    %T")
+        ("m" "memo" entry (file (expand-file-name "~/nushiolab/org/note.org"))
+             "* %?\n    %i\n    %a\n    %T")))
+
+;; agenda
+(setq org-agenda-files (list (expand-file-name "~/nushiolab/org/") (expand-file-name "~/nushiolab/org/gcal/")))
