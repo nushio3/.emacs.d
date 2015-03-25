a6356200ea68d8a672f7f0f7d7f3fbf2877b0a41
Takayuki Muranushi <muranushi@gmail.com>
2015-03-25 14:14:15 +0900

    shell boot shortcut.

--- inits/01-keybinds.el
+++ inits/01-keybinds.el
@@ -4,5 +4,8 @@
 (global-set-key "\C-cs" 'haskell-mode-stylish-buffer)
 (windmove-default-keybindings 'meta)
 
+(global-set-key (kbd "M-s M-s") (lambda() (interactive)(shell-command "gnome-terminal --hide-menubar")))
+
+
 ;; Do not use C-x, C-c, C-v as cut, copy, and paste
 (setq cua-enable-cua-keys  nil)
