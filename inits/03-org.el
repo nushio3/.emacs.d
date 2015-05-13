f739d2d8c38cf3793e588aecf32da7985e99bba5
Takayuki Muranushi <muranushi@gmail.com>
2015-05-13 15:16:59 +0900

    remove all nushiolabe

--- inits/03-org.el
+++ inits/03-org.el
@@ -5,7 +5,7 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 ;(setq org-default-notes-file (concat org-directory "/note.org"))
 (setq org-default-notes-file
-      (expand-file-name "~/nushiolab/org/note.org"))
+      (expand-file-name "~/Dropbox/org/note.org"))
 
 (setq org-deadline-warning-days 7)
 
@@ -16,10 +16,10 @@
 
 ;; capture templates
 (setq org-capture-templates
-      '(("p" "Project Inbox" entry (file+headline (expand-file-name "~/nushiolab/org/inbox.org") "Inbox")
+      '(("p" "Project Inbox" entry (file+headline (expand-file-name "~/Dropbox/org/inbox.org") "Inbox")
                       "** TODO %?\n    %i\n    %a\n    %T")
 ;             "** TODO %?\n    %i\n    %a\n    DEADLINE: %^t")
-        ("n" "note" entry (file (expand-file-name "~/nushiolab/org/note.org"))
+        ("n" "note" entry (file (expand-file-name "~/Dropbox/org/note.org"))
              "* %?\n    %i\n    %a\n    %T")))
 ;; for format, see http://orgmode.org/manual/Template-expansion.html#Template-expansion
 
