ec36bd955579d35100b5fef112cc79e4689d172b
Takayuki Muranushi <muranushi@gmail.com>
2016-09-29 12:18:53 +0900

    Add support for BSD-allman indent

--- inits/90-c.el
+++ inits/90-c.el
@@ -2,6 +2,7 @@
   (when (and buffer-file-name
              (string-match "dlchip-aics" buffer-file-name))
     (progn (c-set-style "bsd")
+           (setq c-basic-offset 2)
            (c-set-offset 'substatement-open 0)
            )
     ))
